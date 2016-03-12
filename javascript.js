cd1d89916e5e6542567bbbb755560e6c3b54f4d2
gia-yutongwu <gia.yutong@outlook.com>
2016-03-12 11:17:51 -0500

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -12,11 +12,78 @@ angular.module('portalApp')
     // Import variables and functions from service
     $scope.data = cosine37Factory.data;
 
+	// Show main view in the first column
+	$scope.portalHelpers.showView('main.html', 1);
+
+    // Import variables and functions from service
+    $scope.loading = cosine37Factory.loading;
+    $scope.item = {value:''};
+
+    // Model for the search and list example
+    $scope.model = [{
+        title: "item 1",
+        details: "item 1 details",
+        category: '1'
+    }, {
+        title: "item 2",
+        details: "item 2 details",
+        category: '2'
+    }, {
+        title: "item 3",
+        details: "item 3 details",
+        category: '1'
+    }, {
+        title: "item 4",
+        details: "item 4 details",
+        category: '2'
+    }, {
+        title: "item 5",
+        details: "item 5 details",
+        category: '1'
+    }, {
+        title: "item 6",
+        details: "item 6 details",
+        category: '2'
+    }];
+
     // initialize the service
     cosine37Factory.init($scope);
 
-	// Show main view in the first column
-	$scope.portalHelpers.showView('main.html', 1);
+    // watch for changes in the loading variable
+    $scope.$watch('loading.value', function () {
+        // if loading
+        if ($scope.loading.value) {
+            // show loading screen in the first column, and don't append it to browser history
+            $scope.portalHelpers.showView('loading.html', 1, false);
+            // show loading animation in place of menu button
+            $scope.portalHelpers.toggleLoading(true);
+        } else {
+            $scope.portalHelpers.showView('main.html', 1);
+            $scope.portalHelpers.toggleLoading(false);
+        }
+    });
+
+    // Handle click on an item in the list and search example
+    $scope.showDetails = function (item) {
+        // Set which item to show in the details view
+        $scope.item.value = item;
+        // Show details view in the second column
+        $scope.portalHelpers.showView('details.html', 2);
+    };
+
+    // Handle "previous item" click from the details page
+    $scope.prevItem = function () {
+        // get previous items in the list
+        var prevItem = $scope.portalHelpers.getPrevListItem();
+        // refresh details view with the new item
+        $scope.showDetails(prevItem);
+    }
+
+    $scope.nextItem = function () {
+        var nextItem = $scope.portalHelpers.getNextListItem();
+        $scope.showDetails(nextItem);
+    }
+
 	
 }])
 // Factory maintains the state of the widget
@@ -26,23 +93,33 @@ angular.module('portalApp')
 
 	// Your variable declarations
 	var data = {value: null};
+    
+    // Your variable declarations
+        var loading = {
+            value: true
+        };
+    
+    var sourcesLoaded = 0;
 
 	var init = function ($scope) {
-		if (initialized.value)
-			return;
-		
-		initialized.value = true;
-
-		// Place your init code here:
-		data.value={message:"Welcome to Portal SDK!"};
-	}
+            if (initialized.value)
+                return;
+            initialized.value = true;
+            // Place your init code here:
+            sourceLoaded();
+        }
 
 
-	// Expose init(), and variables
-	return {
-		init: init,
-		data: data
-	};
+	function sourceLoaded() {
+            sourcesLoaded++;
+            if (sourcesLoaded > 0)
+                loading.value = false;
+        }
+
+        return {
+            init: init,
+            loading: loading
+        };
 
 }])
 // Custom directive example
@@ -61,3 +138,4 @@ angular.module('portalApp')
 		return output;
 	}
 });
+
